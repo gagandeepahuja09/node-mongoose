323d2abae2d0d9d7f15db728c4b66dad3f86244e
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-03-30 23:33:02 +0530

    creating schema and using them

---
+++ index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+	console.log('Connected correctly to server');
+
+	var newDish = Dishes({
+		name: 'Salad',
+		description: 'healthy',
+	});
+
+	newDish.save()
+		.then((dish) => {
+			console.log(dish);
+
+			// find will find all the dishes because no query applied
+			// exec ensures that it is executed
+			return Dishes.find({}).exec();
+		})
+		.then((dishes) => {
+			console.log(dishes);
+			return Dishes.remove({});
+		})
+		.then(() => {
+			return mongoose.connection.close();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
---
+++ models/dishes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dishSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	description: {
+		type: String,
+		required: true,
+	}
+}, {
+		// Will automatically, added created at and update as timestamp
+		timestamps: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
