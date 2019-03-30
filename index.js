9c37395edfa0a9e64f7cf7f6b212515dcbef851b
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-03-31 00:08:40 +0530

    using a schema as a subdocument

--- index.js
+++ index.js
@@ -8,27 +8,25 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
 	console.log('Connected correctly to server');
 
-	var newDish = Dishes({
-		name: 'Salad',
-		description: 'healthy',
+	Dishes.create({
+		name: 'Chicken',
+		description: 'tasty',
+	})
+	.then((dish) => {
+		console.log(dish);
+
+		// find will find all the dishes because no query applied
+		// exec ensures that it is executed
+		return Dishes.find({}).exec();
+	})
+	.then((dishes) => {
+		console.log(dishes);
+		return Dishes.remove({});
+	})
+	.then(() => {
+		return mongoose.connection.close();
+	})
+	.catch((err) => {
+		console.log(err);
 	});
-
-	newDish.save()
-		.then((dish) => {
-			console.log(dish);
-
-			// find will find all the dishes because no query applied
-			// exec ensures that it is executed
-			return Dishes.find({}).exec();
-		})
-		.then((dishes) => {
-			console.log(dishes);
-			return Dishes.remove({});
-		})
-		.then(() => {
-			return mongoose.connection.close();
-		})
-		.catch((err) => {
-			console.log(err);
-		});
 });
--- models/dishes.js
+++ models/dishes.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+	rating: {
+		type: Number,
+		min: 1,
+		max: 1,
+		required: true
+	},
+	comment: {
+		type: String,
+		required: true
+	},
+	author: {
+		type: String,
+		required: true
+	}
+}, {
+	timestamps: true,	
+});
+
 const dishSchema = new Schema({
 	name: {
 		type: String,
@@ -10,7 +29,9 @@ const dishSchema = new Schema({
 	description: {
 		type: String,
 		required: true,
-	}
+	},
+	// Every dish can have multiple documents
+	comments: [	commentSchema	]
 }, {
 		// Will automatically, added created at and update as timestamp
 		timestamps: true
